29ff1628fee686d8547161a0c224437e49df21c8
Mohamed El Masry <91676840+MoDev2002@users.noreply.github.com>
2024-08-04 22:46:52 +0300

    fix: make work together card content centered

--- src/features/about/components/grid-items/work-together.tsx
+++ src/features/about/components/grid-items/work-together.tsx
@@ -25,11 +25,11 @@ const WorkTogether = () => {
         setTimeout(() => setCopied(false), 2000);
     };
     return (
-        <BentoGridItem className='flex justify-center md:col-span-3 md:row-span-1 lg:col-span-2'>
+        <BentoGridItem className='flex items-center justify-center md:col-span-3 md:row-span-1 lg:col-span-2'>
             <BackgroundGradientAnimation>
                 <div className='pointer-events-none absolute inset-0 z-50 flex cursor-default items-center justify-center px-4 text-center text-3xl font-bold text-white md:text-4xl lg:text-6xl'></div>
             </BackgroundGradientAnimation>
-            <BentoGridHeader className='max-w-60 justify-center text-center md:max-w-full'>
+            <BentoGridHeader className='max-w-60 items-center justify-center text-center md:max-w-full'>
                 <BentoGridTitle>
                     Do you want to start a project together?
                 </BentoGridTitle>
